ab1ca97f4987fd34e3e9c86520f4d5ce15a466dc
egluna-dev <egluna@live.ca>
2021-12-24 19:26:23 +1100

    Moved jwt key to .env

--- djevents-backend/config/server.js
+++ djevents-backend/config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'e5f5bd1c6f28dd6bdd363cbedc6e29aa'),
+      secret: env('ADMIN_JWT_SECRET', 'JWT_KEY'),
     },
   },
 });
